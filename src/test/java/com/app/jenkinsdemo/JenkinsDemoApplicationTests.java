ee3a9854327e97b9fb0fadd66ccf538acafa0ac4
Akash K K <Akash.KK@ibm.com>
2024-08-13 00:55:08 +0530

    Commiting changes

--- src/test/java/com/app/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/app/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.app.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,11 +13,11 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class JenkinsDemoApplicationTests {
-
+Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
     @Test
     void contextLoads() {
 
-
+logger.info("Test method executing............");
         assertEquals(true, true);
     }
 
